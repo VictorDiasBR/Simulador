4e1e6ba87854f6af844f6f33823aa3733e03b602
Victor Dias <30852439+VictorDiasBR@users.noreply.github.com>
2021-04-05 07:40:42 -0300

    add simulator stepps front, simulator response layout done

--- src/app/simulador/simulador.component.ts
+++ src/app/simulador/simulador.component.ts
@@ -12,31 +12,83 @@ import {
   Validators
 } from "@angular/forms";
 import { STEPPER_GLOBAL_OPTIONS } from "@angular/cdk/stepper";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+
 @Component({
   selector: "app-simulador",
   templateUrl: "./simulador.component.html",
   styleUrls: ["./simulador.component.css"],
   providers: [
     {
-      provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError: true}
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true }
     }
   ]
 })
 export class SimuladorComponent implements OnInit {
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  range = new FormGroup({
-    start: new FormControl(),
-    end: new FormControl()
-  });
-  constructor(private _formBuilder: FormBuilder) {}
+  dadosIniciais: FormGroup;
+  tipo: FormGroup;
+  periodo: FormGroup;
+  modelo: FormGroup;
+
+  cols: number;
+
+  gridByBreakpoint = {
+    xl: 2,
+    lg: 2,
+    md: 2,
+    sm: 1,
+    xs: 1
+  };
+  constructor(
+    private _formBuilder: FormBuilder,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    this.breakpointObserver
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge
+      ])
+      .subscribe((result) => {
+        if (result.matches) {
+          if (result.breakpoints[Breakpoints.XSmall]) {
+            this.cols = this.gridByBreakpoint.xs;
+          }
+          if (result.breakpoints[Breakpoints.Small]) {
+            this.cols = this.gridByBreakpoint.sm;
+          }
+          if (result.breakpoints[Breakpoints.Medium]) {
+            this.cols = this.gridByBreakpoint.md;
+          }
+          if (result.breakpoints[Breakpoints.Large]) {
+            this.cols = this.gridByBreakpoint.lg;
+          }
+          if (result.breakpoints[Breakpoints.XLarge]) {
+            this.cols = this.gridByBreakpoint.xl;
+          }
+        }
+      });
+  }
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ["", Validators.required]
+    this.dadosIniciais = this._formBuilder.group({
+      nome: ["", Validators.required],
+      descricao: ["", Validators.required]
     });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ["", Validators.required]
+    this.tipo = this._formBuilder.group({
+      tipo: ["", Validators.required]
+    });
+
+    this.periodo = this._formBuilder.group({
+      inicio: ["", Validators.required],
+      fim: ["", Validators.required]
+    });
+
+    this.modelo = this._formBuilder.group({
+      modelo: ["", Validators.required]
     });
   }
 }
